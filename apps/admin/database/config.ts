e1e16cd85df65bd586a8bb6de19211cf4116377f
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2025-08-16 19:31:51 +0300

    updated admin

--- apps/admin/database/config.ts
+++ apps/admin/database/config.ts
@@ -1,34 +1,29 @@
-import { Database } from "./schema"; // this is the Database interface we defined earlier
+import { Database } from "./schema";
 import pkg from "pg";
 const { Pool } = pkg;
 import { Kysely, PostgresDialect, sql } from "kysely";
 
+const pool =
+  process.env.ENVIRON === "local"
+    ? new Pool({
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        port: parseInt(process.env.DB_PORT!),
+        ssl: false,
+        max: 10,
+      })
+    : new Pool({
+        connectionString: process.env.DATABASE_URL, // Render gives you this
+        ssl: {
+          rejectUnauthorized: false, // needed for most managed services
+        },
+        max: 10,
+      });
+
 const dialect = new PostgresDialect({
-  pool: new Pool({
-    database:
-      process.env.ENVIRON === "local"
-        ? process.env.DB_NAME
-        : process.env.DB_PROD_NAME,
-    host:
-      process.env.ENVIRON === "local"
-        ? process.env.DB_HOST
-        : process.env.DB_PROD_HOST,
-    user:
-      process.env.ENVIRON === "local"
-        ? process.env.DB_USER
-        : process.env.DB_PROD_USER,
-    password:
-      process.env.ENVIRON === "local"
-        ? process.env.DB_PASS
-        : process.env.DB_PROD_PASS,
-    port: parseInt(
-      process.env.ENVIRON === "local"
-        ? process.env.DB_PORT!
-        : process.env.DB_PROD_PORT!
-    ),
-    ssl: process.env.ENVIRON === "local" ? false : true,
-    max: 10,
-  }),
+  pool,
 });
 
 export const database = new Kysely<Database>({
